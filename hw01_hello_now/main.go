65dd2f4e36bb25ecdfc08cd8fac6ed8d4837e371
Haba1234 <b_roman@inbox.ru>
2020-11-29 19:33:14 +0300

    HW1 is completed

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
-	// Place your code here
+	d := 1 * time.Second //Defining duration
+
+	// текущее время системы
+	currentTime := time.Now()
+	fmt.Println("current time:", currentTime.Round(d))
+
+	// точное время
+	exactTime, err := ntp.Time("pool.ntp.org")
+	if err != nil {
+		log.Fatalf("ntp error: %s", err)
+	}
+	fmt.Println("exact time:", exactTime.Round(d))
 }
