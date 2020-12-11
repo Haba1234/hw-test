bf94e5832a5d0b919c43c2da1decc90195735b98
Haba1234 <b_roman@inbox.ru>
2020-12-11 19:07:59 +0300

    Работа над замечаниями

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -8,16 +8,15 @@ import (
 )
 
 var ErrInvalidString = errors.New("invalid string")
-var resultStr strings.Builder // готовим построитель для новой строки
 
-func multiChar(char rune, count int) {
-	resultStr.WriteString(strings.Repeat(string(char), count))
+func multiChar(char rune, count int) string {
+	return strings.Repeat(string(char), count)
 }
 
 func checkSymbols(char rune) bool { //nolint:gofmt
 	// проверка, что символ соответствует разрешенному.
 	result := true
-	if !unicode.IsLetter(char) && !unicode.IsDigit(char) && unicode.IsPunct(char) && (char != 92) {
+	if !unicode.IsLetter(char) && !unicode.IsDigit(char) && unicode.IsPunct(char) && (string(char) != `\`) {
 		result = false
 	}
 	return result
@@ -27,6 +26,7 @@ func Unpack(str string) (string, error) { //nolint:gocognit,funlen,gocyclo
 	var prevChar rune
 	var count int
 	var punct, double bool
+	var resultStr strings.Builder // готовим построитель для новой строки
 
 	resultStr.Reset()
 
@@ -37,11 +37,11 @@ func Unpack(str string) (string, error) { //nolint:gocognit,funlen,gocyclo
 
 	// перебор символов строки
 	for index, char := range str {
-		// неразрешенный символ
+		// проверка на неразрешенные символы
 		if !checkSymbols(char) {
 			return "", ErrInvalidString
 		}
-		// строка начинается с цифры или \ - ошибка
+		// строка начинается с цифры или \ - ошибка. Остальные символы отсеиваются в функции checkSymbols
 		if index == 0 && (unicode.IsDigit(char) || unicode.IsPunct(char)) {
 			return "", ErrInvalidString
 		}
@@ -50,19 +50,19 @@ func Unpack(str string) (string, error) { //nolint:gocognit,funlen,gocyclo
 			switch {
 			case unicode.IsLetter(char) && unicode.IsLetter(prevChar):
 				// буква после буквы
-				multiChar(prevChar, 1)
+				resultStr.WriteString(multiChar(prevChar, 1))
 			case unicode.IsPunct(char) && unicode.IsLetter(prevChar):
 				// \ после буквы
-				multiChar(prevChar, 1)
+				resultStr.WriteString(multiChar(prevChar, 1))
 				punct = true
 			case unicode.IsPunct(char) && unicode.IsDigit(prevChar) && punct:
 				// \ после цифры
-				multiChar(prevChar, 1)
+				resultStr.WriteString(multiChar(prevChar, 1))
 				punct = true
 			case unicode.IsPunct(char) && unicode.IsPunct(prevChar):
 				// \ после \
 				if double {
-					multiChar(prevChar, 1)
+					resultStr.WriteString(multiChar(prevChar, 1))
 					double = false
 				} else {
 					double = true
@@ -71,7 +71,7 @@ func Unpack(str string) (string, error) { //nolint:gocognit,funlen,gocyclo
 			case (unicode.IsDigit(char) && unicode.IsLetter(prevChar)) || (unicode.IsDigit(char) && unicode.IsDigit(prevChar) && punct) || double:
 				// цифра после буквы, размножение символа
 				count, _ = strconv.Atoi(string(char))
-				multiChar(prevChar, count)
+				resultStr.WriteString(multiChar(prevChar, count))
 				punct = false
 			case unicode.IsDigit(char) && unicode.IsPunct(prevChar):
 				// цифра после \
@@ -84,7 +84,7 @@ func Unpack(str string) (string, error) { //nolint:gocognit,funlen,gocyclo
 
 	if unicode.IsLetter(prevChar) || punct {
 		// сохранить последний символ в строке перед возвратом результата
-		multiChar(prevChar, 1)
+		resultStr.WriteString(multiChar(prevChar, 1))
 	}
 
 	return resultStr.String(), nil
