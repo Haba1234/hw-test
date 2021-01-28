42aa6ffcf8cd427e640c6f5d6077dcda562df2c8
Haba1234 <b_roman@inbox.ru>
2021-01-28 12:57:11 +0300

    fix linters

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -6,16 +6,16 @@ import (
 type Key string
 
 type Cache interface {
-	Set(key Key, value interface{}) bool // Добавить значение в кэш по ключу
-	Get(key Key) (interface{}, bool)     // Получить значение из кэша по ключу
-	Clear()                              // Очистить кэш
+	Set(key Key, value interface{}) bool // Добавить значение в кэш по ключу.
+	Get(key Key) (interface{}, bool)     // Получить значение из кэша по ключу.
+	Clear()                              // Очистить кэш.
 }
 
 type lruCache struct {
 	mx       sync.RWMutex
-	capacity int               // ёмкость (количество сохраняемых в кэше элементов)
-	queue    List              // очередь [последних используемых элементов] на основе двусвязного списка
-	items    map[Key]*ListItem // словарь, отображающий ключ (строка) на элемент очереди
+	capacity int               // ёмкость (количество сохраняемых в кэше элементов).
+	queue    List              // очередь [последних используемых элементов] на основе двусвязного списка.
+	items    map[Key]*ListItem // словарь, отображающий ключ (строка) на элемент очереди.
 }
 
 type cacheItem struct {
@@ -31,57 +31,56 @@ func NewCache(capacity int) Cache {
 	}
 }
 
-// Добавить значение в кэш по ключу
+// Добавить значение в кэш по ключу.
 func (lru *lruCache) Set(key Key, value interface{}) bool {
-	lru.mx.Lock()         // включаем защиту при параллельном доступе
-	defer lru.mx.Unlock() // выключаем защиту перед выходом из функции
+	lru.mx.Lock()         // включаем защиту при параллельном доступе.
+	defer lru.mx.Unlock() // выключаем защиту перед выходом из функции.
 
 	objValue := cacheItem{key: key, value: value}
 	_, ok := lru.items[key]
-	lru.items[key] = &ListItem{Value: objValue} // обновляем значение в словаре или добавляем в словарь
+	lru.items[key] = &ListItem{Value: objValue} // обновляем значение в словаре или добавляем в словарь.
 	if !ok {
 		// если элемента нет в словаре, то добавить в словарь и в начало очереди
 		// (при этом, если размер очереди больше ёмкости кэша, то необходимо
-		// удалить последний элемент из очереди и его значение из словаря)
-		if lru.queue.Len() == lru.capacity { // очередь достигла максимума
-			valBack := lru.queue.Back() // крайний элемент в списке
+		// удалить последний элемент из очереди и его значение из словаря).
+		if lru.queue.Len() == lru.capacity { // очередь достигла максимума.
+			valBack := lru.queue.Back() // крайний элемент в списке.
 
-			lru.queue.Remove(valBack)                        // удалить крайний элемент из списка
-			delete(lru.items, valBack.Value.(cacheItem).key) // удалить элемент из словаря
+			lru.queue.Remove(valBack)                        // удалить крайний элемент из списка.
+			delete(lru.items, valBack.Value.(cacheItem).key) // удалить элемент из словаря.
 		}
-		lru.queue.PushFront(objValue) // добавить элемент в начало списка
+		lru.queue.PushFront(objValue) // добавить элемент в начало списка.
 		return false
 	}
 
 	// если элемент присутствует в словаре, то обновить его значение
-	// и переместить элемент в начало очереди
+	// и переместить элемент в начало очереди.
 	lru.queue.MoveToFront(lru.items[key])
 	return true
 }
 
-// Получить значение из кэша по ключу
+// Получить значение из кэша по ключу.
 func (lru *lruCache) Get(key Key) (interface{}, bool) {
-	lru.mx.RLock()         // включаем защиту при параллельном доступе
-	defer lru.mx.RUnlock() // выключаем защиту перед выходом из функции
+	lru.mx.RLock()         // включаем защиту при параллельном доступе.
+	defer lru.mx.RUnlock() // выключаем защиту перед выходом из функции.
 
 	if elem, ok := lru.items[key]; ok {
 		// если элемент присутствует в словаре, то переместить элемент
-		// в начало очереди и вернуть его значение и true
+		// в начало очереди и вернуть его значение и true.
 		lru.queue.MoveToFront(lru.items[key])
 		return elem.Value.(cacheItem).value, true
 	}
-	// если элемента нет в словаре, то вернуть nil и false
+	// если элемента нет в словаре, то вернуть nil и false.
 	return nil, false
 }
 
-// Очистить кэш
+// Очистить кэш.
 func (lru *lruCache) Clear() {
-	lru.mx.Lock()         // включаем защиту при параллельном доступе
-	defer lru.mx.Unlock() // выключаем защиту перед выходом из функции
+	lru.mx.Lock()         // включаем защиту при параллельном доступе.
+	defer lru.mx.Unlock() // выключаем защиту перед выходом из функции.
 
-	lru.items = make(map[Key]*ListItem, lru.capacity) // очистка кэша
-	for i := 0; i < lru.queue.Len(); i++ {            // очистка очереди
+	lru.items = make(map[Key]*ListItem, lru.capacity) // очистка кэша.
+	for i := 0; i < lru.queue.Len(); i++ {            // очистка очереди.
 		lru.queue.Remove(lru.queue.Back())
 	}
-	return
 }
--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -26,12 +26,12 @@ func NewList() List {
 	return new(list)
 }
 
-// вычисление длины списка
+// вычисление длины списка.
 func (l *list) Len() int {
 	return len(l.elements)
 }
 
-// вернуть адрес первого элемента списка или nil, если список пуст
+// вернуть адрес первого элемента списка или nil, если список пуст.
 func (l *list) Front() *ListItem {
 	if result := l.Len(); result > 0 {
 		return l.elements[0]
@@ -39,7 +39,7 @@ func (l *list) Front() *ListItem {
 	return nil
 }
 
-// вернуть адрес последнего элемента списка или nil, если список пуст
+// вернуть адрес последнего элемента списка или nil, если список пуст.
 func (l *list) Back() *ListItem {
 	if result := l.Len(); result > 0 {
 		return l.elements[result-1]
@@ -47,36 +47,36 @@ func (l *list) Back() *ListItem {
 	return nil
 }
 
-// добавить значение в начало списка
+// добавить значение в начало списка.
 func (l *list) PushFront(v interface{}) *ListItem {
-	// первая запись? создать певвый элемент списка
+	// первая запись? создать первый элемент списка.
 	if l.Front() == nil {
 		l.elements = append(l.elements, &ListItem{Value: v, Next: nil, Prev: nil})
 		return l.Front()
 	}
 
-	// добавим новый элемент в начало списка
-	newSlice := []*ListItem{{Value: v, Next: l.Front(), Prev: nil}} // создали слайс с новым элементом списка
-	l.elements = append(newSlice, l.elements...)                    // пееенесли в новый слайс все элементы старого
-	l.elements[1].Prev = l.Front()                                  // обновили адрес в Prev для следующего элемента списка
+	// добавим новый элемент в начало списка.
+	newSlice := []*ListItem{{Value: v, Next: l.Front(), Prev: nil}} // создали слайс с новым элементом списка.
+	l.elements = append(newSlice, l.elements...)                    // перенесли в новый слайс все элементы старого.
+	l.elements[1].Prev = l.Front()                                  // обновили адрес в Prev для следующего элемента списка.
 	return l.Front()
 }
 
-// добавить значение в конец списка
+// добавить значение в конец списка.
 func (l *list) PushBack(v interface{}) *ListItem {
-	// проеерка, что список не пустой
+	// проверка, что список не пустой.
 	if prev := l.Back(); prev != nil {
-		// добавить значение в конец
+		// добавить значение в конец.
 		l.elements = append(l.elements, &ListItem{Value: v, Next: nil, Prev: prev})
-		l.elements[l.Len()-2].Next = l.Back() // у предыдущей записи обновить ссылку Next на новый элемент списка
+		l.elements[l.Len()-2].Next = l.Back() // у предыдущей записи обновить ссылку Next на новый элемент списка.
 		return l.Back()
 	}
 
-	// список пуст, создать первую запись
+	// список пуст, создать первую запись.
 	return l.PushFront(v)
 }
 
-// удалить элемент списка
+// удалить элемент списка.
 func (l *list) Remove(i *ListItem) {
 	// список пуст!
 	listLen := l.Len()
@@ -85,9 +85,9 @@ func (l *list) Remove(i *ListItem) {
 	}
 	// проверка, элемент последний в списке?
 	if l.Back().Value == i.Value {
-		l.elements = l.elements[:listLen-1] // обрезаем слайс на крайний элемент
+		l.elements = l.elements[:listLen-1] // обрезаем слайс на крайний элемент.
 		if l.Len() > 0 {
-			// обнуляем ссылку у теперь уже нового крайнего элемента, если он еще остался
+			// обнуляем ссылку у теперь уже нового крайнего элемента, если он еще остался.
 			l.Back().Next = nil
 		}
 		return
@@ -95,13 +95,13 @@ func (l *list) Remove(i *ListItem) {
 	// удаляемый элемент не последний в списке. Ищем где он...
 	for num, val := range l.elements {
 		if val.Value == i.Value {
-			l.elements = append(l.elements[:num], l.elements[num+1:]...) // удаление
-			// обновление ссылкок у предыдущего и следующего элементов списка
+			l.elements = append(l.elements[:num], l.elements[num+1:]...) // удаление.
+			// обновление ссылок у предыдущего и следующего элементов списка.
 			if num > 0 {
 				l.elements[num-1].Next = l.elements[num]
 				l.elements[num].Prev = l.elements[num-1]
 			} else {
-				// если удаляемый элемент первый, только обнулить ссылку у следующего
+				// если удаляемый элемент первый, только обнулить ссылку у следующего.
 				l.elements[num].Prev = nil
 			}
 			return
@@ -109,14 +109,14 @@ func (l *list) Remove(i *ListItem) {
 	}
 }
 
-// переместить элемент в начало списка
+// переместить элемент в начало списка.
 func (l *list) MoveToFront(i *ListItem) {
 	// ищем элемент для перемещения в списке...
 	for num, val := range l.elements {
-		// проверяем, что он не первый
+		// проверяем, что он не первый.
 		if (val.Value == i.Value) && (num > 0) {
-			l.Remove(i)          // удаляем из текущей позиции
-			l.PushFront(i.Value) // и вставляем значение в начало списка
+			l.Remove(i)          // удаляем из текущей позиции.
+			l.PushFront(i.Value) // и вставляем значение в начало списка.
 			return
 		}
 	}
