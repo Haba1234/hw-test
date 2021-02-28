169fe5c10a6f75f81c7c670a6a4e7d9359636508
Haba1234 <b_roman@inbox.ru>
2021-02-28 15:14:49 +0300

    HW08 is completed

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,48 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	expected := make(Environment)
+	expected["BAR"] = EnvValue{"bar", false}
+	expected["EMPTY"] = EnvValue{"", false}
+	expected["FOO"] = EnvValue{"   foo\nwith new line", false}
+	expected["HELLO"] = EnvValue{"\"hello\"", false}
+	expected["UNSET"] = EnvValue{"", true}
+
+	t.Run("map comparison", func(t *testing.T) {
+		dir := "./testdata/env"
+		mapEnv, err := ReadDir(dir)
+		require.Nil(t, err)
+		require.Equal(t, expected, mapEnv, "maps don't match")
+
+	})
+
+	t.Run("path error", func(t *testing.T) {
+		dir := "env"
+		_, err := ReadDir(dir)
+		require.NotNil(t, err)
+	})
+
+	t.Run("filename contains character '='", func(t *testing.T) {
+		envTest, err := ioutil.TempFile("./testdata/env", "out=*")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer os.Remove(envTest.Name())
+
+		dir := "./testdata/env"
+		mapEnv, err := ReadDir(dir)
+
+		require.Nil(t, err)
+		require.Equal(t, expected, mapEnv, "maps don't match")
+	})
 }
