2e697b253590c5aab5c96ca57412abe025748e3f
Haba1234 <b_roman@inbox.ru>
2021-03-17 21:45:06 +0300

    HW11 test

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,62 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"sync"
+	"time"
+)
+
 func main() {
 	// Place your code here,
 	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	timeout := flag.String("timeout", "10s", "timeout connection")
+	host := flag.String("host", "127.0.0.1", "name host or ip address")
+	port := flag.String("port", "4242", "port number")
+	flag.Parse()
+
+	tim, err := time.ParseDuration(*timeout)
+	if err != nil {
+		return
+	}
+	tc := NewTelnetClient(net.JoinHostPort(*host, *port), tim, os.Stdin, os.Stdout)
+	err = tc.Connect()
+	if err != nil {
+		_, err = io.WriteString(os.Stderr, fmt.Sprintf("Error: %v\n", err))
+		if err != nil {
+			log.Fatalln(err)
+		}
+		return
+	}
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		err := tc.Receive()
+		if err != nil {
+			_, err = io.WriteString(os.Stderr, fmt.Sprintf("Error: %v\n", err))
+			if err != nil {
+				log.Fatalln(err)
+			}
+		}
+		tc.Close()
+	}()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		err := tc.Send()
+		if err != nil {
+			_, err = io.WriteString(os.Stderr, fmt.Sprintf("Error: %v\n", err))
+			if err != nil {
+				log.Fatalln(err)
+			}
+		}
+		tc.Close()
+	}()
+	wg.Wait()
+
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"bufio"
+	"context"
+	"fmt"
 	"io"
+	"net"
+	"os"
 	"time"
 )
 
@@ -12,10 +17,92 @@ type TelnetClient interface {
 	Receive() error
 }
 
+type telnetClient struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
+	ctx     context.Context
+}
+
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &telnetClient{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
+
+func (t *telnetClient) Connect() error {
+	//ctx := context.Background()
+	var err error
+	//t.ctx, _ = context.WithTimeout(context.Background(), t.timeout*time.Second)
+	//dialer := &net.Dialer{}
+	//t.conn, err = dialer.DialContext(t.ctx, "tcp", t.address)
+	//dialer.Timeout = 1*time.Millisecond
+	t.conn, err = net.DialTimeout("tcp", t.address, t.timeout)
+	//t.conn, err = net.Dial("tcp", t.address)
+	if err != nil {
+		return fmt.Errorf("cannot connect to %s. Error: %w", t.address, err)
+	}
+	_, err = io.WriteString(os.Stderr, "...Connected to "+t.address+"\n")
+	if err != nil {
+		return fmt.Errorf("cannot write a string. Error: %w", err)
+	}
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (t *telnetClient) Send() error {
+	scanner := bufio.NewScanner(t.in)
+	for {
+		if !scanner.Scan() {
+			_, err := io.WriteString(os.Stderr, "...EOF\n")
+			if err != nil {
+				return fmt.Errorf("cannot write a string. Error: %w", err)
+			}
+			break
+		}
+		str := scanner.Text()
+		if err := scanner.Err(); err != nil {
+			return fmt.Errorf("error reading from channel %v. Error: %w", t.in, err)
+		}
+
+		_, err := t.conn.Write([]byte(fmt.Sprintf("%s\n", str)))
+		if err != nil {
+			_, err := io.WriteString(os.Stderr, "...Connection was closed by peer\n")
+			if err != nil {
+				return fmt.Errorf("cannot write a string. Error: %w", err)
+			}
+			return nil
+		}
+	}
+	return nil
+}
+
+func (t *telnetClient) Receive() error {
+	scanner := bufio.NewScanner(t.conn)
+	for {
+		if !scanner.Scan() {
+			break
+		}
+		text := scanner.Text()
+		if err := scanner.Err(); err != nil {
+			return fmt.Errorf("error reading from channel %v. Error: %w", t.conn, err)
+		}
+		_, err := io.WriteString(t.out, text+"\n")
+		if err != nil {
+			return fmt.Errorf("cannot write a string. Error: %w", err)
+		}
+	}
+	return nil
+}
+
+func (t *telnetClient) Close() error {
+	err := t.conn.Close()
+	if err != nil {
+		return err
+	}
+	return nil
+}
